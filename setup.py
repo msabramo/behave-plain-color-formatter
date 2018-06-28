32810e7f26db874eee98d7aae51b4e72100434f8
Marc Abramowitz <abramowi@adobe.com>
2018-06-28 16:10:11 -0700

    Add long_description, 0.0.1

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='behave-plain-color-formatter',
-    version="0.0.0",
+    version="0.0.1",
     packages=['behave_plain_color_formatter', ],
     url='https://github.com/msabramo/behave-plain-color-formatter',
     license='MIT',
     author='Marc Abramowitz',
     author_email='msabramo@gmail.com',
     description='Formatter for behave that uses color but not fancy terminal repositioning',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=["behave>=1.2.5,<=1.3"],
     keywords=['testing', 'behave', 'teamcity', 'formatter', 'report'],
     classifiers=[
